0f83ebc0cd39cdbbcef9a654832acdf732334f59
Kevin Gibson <rkevingibson@gmail.com>
2021-12-11 17:47:24 -0800

    Day 2 done

--- src/day2.rs
+++ src/day2.rs
@@ -1,13 +1,60 @@
+#[derive(Debug)]
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
 
+fn parse_input(input: &str) -> Vec<(Direction, i32)> {
+    input
+        .lines()
+        .map(|line| {
+            let mut iter = line.split_whitespace();
+            let dir = match iter.next() {
+                Some("forward") => Some(Direction::Forward),
+                Some("down") => Some(Direction::Down),
+                Some("up") => Some(Direction::Up),
+                _ => None,
+            };
+            let count = iter.next().map(|s| s.parse::<i32>().unwrap());
+            (dir.unwrap(), count.unwrap())
+        })
+        .collect()
+}
+
+fn part1(input: &[(Direction, i32)]) -> i32 {
+    let mut state = (0, 0);
+    for ele in input {
+        match ele.0 {
+            Direction::Forward => state.0 += ele.1,
+            Direction::Down => state.1 += ele.1,
+            Direction::Up => state.1 -= ele.1,
+        }
+    }
+    state.0 * state.1
+}
+
+fn part2(input: &[(Direction, i32)]) -> i32 {
+    // State = horizontal position, depth, aim
+    let mut state = (0, 0, 0);
+    for ele in input {
+        match ele.0 {
+            Direction::Forward => {
+                state.0 += ele.1;
+                state.1 += ele.1 * state.2
+            }
+            Direction::Down => state.2 += ele.1,
+            Direction::Up => state.2 -= ele.1,
+        }
+    }
+    state.0 * state.1
+}
 
 fn main() {
     let input = include_str!("../inputs/day2.txt");
-    let values: Vec<i32> = input
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect();
-    println!("Part 1: {}", part1(&values));
-    println!("Part 2: {}", part2(&values))
+    let input = parse_input(input);
+    println!("Part 1: {:?}", part1(&input));
+    println!("Part 2: {:?}", part2(&input));
 }
 
 #[test]
@@ -18,4 +65,8 @@ fn test() {
     up 3
     down 8
     forward 2"#;
+    let input = parse_input(input);
+    println!("Test input: {:?}", input);
+    assert_eq!(part1(&input), 150);
+    assert_eq!(part2(&input), 900);
 }
