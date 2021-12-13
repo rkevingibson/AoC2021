835b6274344b1471b5c488c0741b574f84c703a4
Kevin Gibson <rkevingibson@gmail.com>
2021-12-12 18:58:19 -0800

    Not super pleased with it, but day 4 is done

--- src/day4.rs
+++ src/day4.rs
@@ -1,40 +1,137 @@
 struct Board {
     nums: [i32; 25],
-    set: [bool; 25],
+    set: u32,
 }
 
 impl Board {
+    fn new(nums: &[i32]) -> Board {
+        assert!(nums.len() == 25);
+        let mut b = Board {
+            nums: Default::default(),
+            set: 0,
+        };
+        b.nums.copy_from_slice(nums);
+        b
+    }
+
+    fn reset(&mut self) {
+        self.set = 0;
+    }
+
     fn set_num(&mut self, n: i32) {
         let pos = self.nums.iter().position(|x| *x == n);
         match pos {
-            Some(i) => self.set[i] = true,
+            Some(i) => self.set |= 1 << i,
             None => (),
         }
     }
 
     fn is_winner(&self) -> bool {
-        //if (set)
-        false
+        let wins = [
+            0b00000_00000_00000_00000_11111u32,
+            0b00000_00000_00000_11111_00000u32,
+            0b00000_00000_11111_00000_00000u32,
+            0b00000_11111_00000_00000_00000u32,
+            0b11111_00000_00000_00000_00000u32,
+            0b10000_10000_10000_10000_10000u32,
+            0b01000_01000_01000_01000_01000u32,
+            0b00100_00100_00100_00100_00100u32,
+            0b00010_00010_00010_00010_00010u32,
+            0b00001_00001_00001_00001_00001u32,
+        ];
+        wins.iter().any(|w| *w & self.set == *w)
+    }
+
+    fn score(&self) -> i32 {
+        let mut sum = 0;
+        for i in 0..25 {
+            if self.set & (1 << i) == 0 {
+                sum += self.nums[i];
+            }
+        }
+        sum
     }
 }
 
-fn parse_input(s: &str) {
+fn parse_input(s: &str) -> (Vec<i32>, Vec<Board>) {
     // First line is the inputs, followed by boards
+    let mut lines = s.lines();
+    let calls = lines
+        .next()
+        .map(|line| {
+            line.split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .unwrap();
+    // We get 5 numbers per line, need to collect 25
+    let mut boards = Vec::<Board>::new();
+    let mut nums = Vec::<i32>::new();
+    for line in lines {
+        line.split_whitespace()
+            .map(|s| s.parse::<i32>())
+            .for_each(|el| {
+                if let Ok(e) = el {
+                    nums.push(e);
+                }
+            });
+        if nums.len() == 25 {
+            boards.push(Board::new(&nums));
+            nums.clear();
+        }
+    }
+
+    (calls, boards)
 }
 
-fn part1(input: &[i32]) -> i32 {
+fn part1(calls: &[i32], boards: &mut [Board]) -> i32 {
+    for &call in calls {
+        for i in 0..boards.len() {
+            boards[i].set_num(call);
+        }
+        let winner = boards.iter().find(|b| b.is_winner());
+        if let Some(winning_board) = winner {
+            println!("Called {}", call);
+            println!("Board score {}", winning_board.score());
+            return winning_board.score() * call;
+        }
+    }
     0
 }
 
-fn part2(input: &[i32]) -> i32 {
+fn part2(calls: &[i32], mut boards: Vec<Board>) -> i32 {
+    for &call in calls {
+        for i in 0..boards.len() {
+            boards[i].set_num(call);
+        }
+        boards = boards.into_iter().filter(|b| !b.is_winner()).collect();
+
+        if boards.len() == 1 {
+            // Final board, need to find score when we lose:
+            break;
+        }
+    }
+
+    let losing_board = &mut boards[0];
+    losing_board.reset();
+    for &call in calls {
+        losing_board.set_num(call);
+        if losing_board.is_winner() {
+            return losing_board.score() * call;
+        }
+    }
     0
 }
 
 fn main() {
     let input = include_str!("../inputs/day4.txt");
+    let (calls, mut boards) = parse_input(input);
     // Parse input here
-    println!("Part 1: {}", part1(&values));
-    println!("Part 2: {}", part2(&values))
+    println!("Part 1: {}", part1(&calls, &mut boards));
+    for ele in &mut boards {
+        ele.reset();
+    }
+    println!("Part 2: {}", part2(&calls, boards));
 }
 
 #[test]
@@ -58,6 +155,11 @@ fn test_case() {
     18  8 23 26 20
     22 11 13  6  5
      2  0 12  3  7"#;
-    assert_eq!(part1(&input), 7);
-    assert_eq!(part2(&input), 5);
+    let (calls, mut boards) = parse_input(input);
+
+    assert_eq!(part1(&calls, &mut boards), 4512);
+    for ele in &mut boards {
+        ele.reset();
+    }
+    assert_eq!(part2(&calls, boards), 1924);
 }
