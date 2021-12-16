6c7b0b1559c054c51ae3901a8a47a2215ffee2aa
Kevin Gibson <rkevingibson@gmail.com>
2021-12-15 21:09:27 -0800

    Finished day 9

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::thread::current;
+
 fn parse_input(s: &str) -> Vec<Vec<u8>> {
     s.lines()
         .map(|line| {
@@ -17,7 +19,7 @@ fn part1(input: &[Vec<u8>]) -> i32 {
         for c in 0..cols {
             // Can safely get, then unwrap with a default hi value to make the test pass
             let up = if r > 0 { input[r - 1][c] } else { 10 };
-            let down = input.get(r + 1).map_or(10,|row| row[c]);
+            let down = input.get(r + 1).map_or(10, |row| row[c]);
             let left = if c > 0 { input[r][c - 1] } else { 10 };
             let &right = input[r].get(c + 1).unwrap_or(&10);
             let x = input[r][c];
@@ -29,8 +31,64 @@ fn part1(input: &[Vec<u8>]) -> i32 {
     risk_level
 }
 
-fn part2(input: &[Vec<u8>]) -> i32 {
-    0
+fn part2(input: &[Vec<u8>]) -> usize {
+    let mut basin_ids: Vec<Vec<Option<u32>>> = Vec::new();
+    basin_ids.resize_with(input.len(), || {
+        let mut v = Vec::<Option<u32>>::new();
+        v.resize_with(input[0].len(), || None);
+        v
+    });
+
+    let mut stack: Vec<(usize, usize)> = Vec::new();
+    let size_x = input.len();
+    let size_y = input[0].len();
+
+    let mut basin_count = 0;
+
+    let mut sizes = Vec::<usize>::new();
+
+    for col in 0..size_x {
+        for row in 0..size_y {
+            if basin_ids[col][row].is_none() {
+                basin_ids[col][row] = Some(basin_count);
+                sizes.push(0);
+                stack.push((col, row));
+                basin_count += 1;
+            }
+
+            while let Some((x, y)) = stack.pop() {
+                let current_id = basin_ids[x][y].unwrap();
+                if x >= 1 && basin_ids[x - 1][y].is_none() && input[x - 1][y] != 9 {
+                    basin_ids[x - 1][y] = Some(current_id);
+                    sizes[current_id as usize] += 1;
+                    stack.push((x - 1, y));
+                }
+
+                if x + 1 < size_x && basin_ids[x + 1][y].is_none() && input[x + 1][y] != 9 {
+                    basin_ids[x + 1][y] = Some(current_id);
+                    sizes[current_id as usize] += 1;
+                    stack.push((x + 1, y));
+                }
+
+                if y >= 1 && basin_ids[x][y - 1].is_none() && input[x][y - 1] != 9 {
+                    basin_ids[x][y - 1] = Some(current_id);
+                    sizes[current_id as usize] += 1;
+                    stack.push((x, y - 1));
+                }
+
+                if y + 1 < size_y && basin_ids[x][y + 1].is_none() && input[x][y + 1] != 9 {
+                    basin_ids[x][y + 1] = Some(current_id);
+                    sizes[current_id as usize] += 1;
+                    stack.push((x, y + 1));
+                }
+            }
+        }
+    }
+
+
+    sizes.sort();
+    let len = sizes.len();
+    sizes[len - 1] * sizes[len - 2] * sizes[len-3]
 }
 
 fn main() {
@@ -50,5 +108,5 @@ fn test_case() {
     let input = parse_input(input);
     println!("{:?}", input);
     assert_eq!(part1(&input), 15);
-    //assert_eq!(part2(&input), 5);
+    assert_eq!(part2(&input), 1134);
 }
